c71f6d068a1baddb2a55918485639176facbdcf3
Fujii Rise <momozato777@gmail.com>
2023-11-15 18:02:47 +0900

    経歴を削除してUIを調整しました

--- src/App.jsx
+++ src/App.jsx
@@ -134,50 +134,8 @@ function App() {
                     </div>
                 </div>
               </div>
-              {/* 経歴 */}
-              <div className="md:w-1/4 p-4">
-                <div className="bg-gray-200 rouded-lg p-8">
-                  <div className="flex items-center mb-3">
-                    <div className="bg-green-500 text-white rounded-full w-8 h-8 flex justify-center items-center mr-3">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M11 10.9794C11 10.4271 11.4477 9.97937 12 9.97937C12.5523 9.97937 13 10.4271 13 10.9794V16.9794C13 17.5317 12.5523 17.9794 12 17.9794C11.4477 17.9794 11 17.5317 11 16.9794V10.9794Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M12 6.05115C11.4477 6.05115 11 6.49886 11 7.05115C11 7.60343 11.4477 8.05115 12 8.05115C12.5523 8.05115 13 7.60343 13 7.05115C13 6.49886 12.5523 6.05115 12 6.05115Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    </div>
-                    <h2 className="text-gray-900 text-lg font-medium">経歴</h2>
-                    </div>
-                    <div>
-                    <p>
-                      高校<br/>
-                      安田女子中学高等学校 普通科<br/>
-                      (2017/4 - 2020/3)<br/>
-                      <br/>
-                      中学<br/>
-                        吹奏楽部 (2014/4 - 2016/8)
-                    </p>
-                    </div>
-                </div>
-              </div>
               {/* 参加歴 */}
-              <div className="md:w-1/4 p-4">
+              <div className="md:w-1/3 p-4">
                 <div className="bg-gray-200 rouded-lg p-8">
                   <div className="flex items-center mb-3">
                     <div className="bg-green-500 text-white rounded-full w-8 h-8 flex justify-center items-center mr-3">
@@ -215,7 +173,7 @@ function App() {
                 </div>
               </div>
               {/* アカウント */}
-              <div className="md:w-1/4 p-4">
+              <div className="md:w-1/3 p-4">
                 <div className="bg-gray-200 rouded-lg p-8">
                   <div className="flex items-center mb-3">
                     <div className="bg-green-500 text-white rounded-full w-8 h-8 flex justify-center items-center mr-3">
@@ -246,7 +204,7 @@ function App() {
                 </div>
               </div>
               {/* 資格 */}
-              <div className="md:w-1/4 p-4">
+              <div className="md:w-1/3 p-4">
                 <div className="bg-gray-200 rouded-lg p-8">
                   <div className="flex items-center mb-3">
                     <div className="bg-green-500 text-white rounded-full w-8 h-8 flex justify-center items-center mr-3">
